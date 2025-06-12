0e571e1e13f14779ed1b81a2857cffb59be73584
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2025-06-12 18:43:14 +0700

    fix: load correct env during backend integration test (#2)

--- apps/server/test/test-helpers/setup-integration.ts
+++ apps/server/test/test-helpers/setup-integration.ts
@@ -1,7 +1,15 @@
 import { configApp } from '@/app';
+// eslint-disable-next-line import/order
+import { resolve } from 'path';
+
+// Env must be configured before importing `AppModule`, to ensure that
+// correct environment variables are loaded.
+config({ path: resolve(__dirname, '../../.env.integration'), override: true });
+
 import { AppModule } from '@/app.module';
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { config } from 'dotenv';
 import { Server } from 'net';
 
 let integrationTestModule: TestingModule;
